77b33c66314296a9ccfcc73cc7ae6ef445d2386c
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 03:37:40 +0000

    Nougat MR1: Advanced restart menu
    Based loosely on this commit: https://github.com/krexus/android_frameworks_base/commit/63284a9d6e98fedfd93c1a5bd24a736f942e3d51
    and on the "Add restart to GlobalActions" commit by Nougat-MR1
    by @KreAch3R
    
    Including
    * Fixes to longpress behavior
    * Removal of "soft-reboot"
    * Restart option is now fixed and can't be removed (only the advanced menu can be toggled)
    * Removed longpress "reboot to safe mode" from power menu (left it only on restart)
    * Enabled confirm on new Restart code
    
    Change-Id: Ibce551a4898080ef7f150b566b53a40a8a14277f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6436,6 +6436,12 @@ public final class Settings {
         public static final String CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED =
                 "camera_double_tap_power_gesture_disabled";
 
+	/**
+         * Whether to include options in power menu for rebooting into recovery or bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
         /**
          * Whether the camera double twist gesture to flip between front and back mode should be
          * enabled.
@@ -6714,7 +6720,8 @@ public final class Settings {
             DOZE_PULSE_ON_PICK_UP,
             DOZE_PULSE_ON_DOUBLE_TAP,
             STATUS_BAR_SHOW_BATTERY_PERCENT,
-            STATUS_BAR_BATTERY_STYLE
+            STATUS_BAR_BATTERY_STYLE,
+            ADVANCED_REBOOT
         };
 
         /**
