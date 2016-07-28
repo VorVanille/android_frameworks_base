f092dee355b9bf4420cf8c9696035c1151aec6fa
Steve Kondik <steve@cyngn.com>
2016-07-28 05:51:11 -0700

    lights: Make sure the lights are out
     * Annoying situation where the battery charging LED comes on in
       the crypto startup screen, but never gets turned off because
       of caching.
     * Explicitly turn off LEDs after the bootanimation completes, and
       always set the full state to the hardware on the first call
       regardless if it's all zeros.
    
    Change-Id: I8cd2325ec5d8924391e4fc62249f6dd737d52f3e

--- services/core/java/com/android/server/lights/LightsService.java
+++ services/core/java/com/android/server/lights/LightsService.java
@@ -124,10 +124,11 @@ public class LightsService extends SystemService {
 
         private void setLightLocked(int color, int mode, int onMS, int offMS, int brightnessMode) {
             if (!mLocked && (color != mColor || mode != mMode || onMS != mOnMS || offMS != mOffMS ||
-                    mBrightnessMode != brightnessMode)) {
+                    mBrightnessMode != brightnessMode || mReset)) {
                 if (DEBUG) Slog.v(TAG, "setLight #" + mId + ": color=#"
                         + Integer.toHexString(color) + ": brightnessMode=" + brightnessMode);
                 mLastColor = mColor;
+                mReset = false;
                 mColor = color;
                 mMode = mode;
                 mOnMS = onMS;
@@ -159,6 +160,7 @@ public class LightsService extends SystemService {
         private boolean mVrModeEnabled;
         private boolean mUseLowPersistenceForVR;
         private boolean mLocked;
+        private boolean mReset = true;
     }
 
     public LightsService(Context context) {
