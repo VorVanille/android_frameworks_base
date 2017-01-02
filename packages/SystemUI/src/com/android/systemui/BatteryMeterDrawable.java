30a56f8ddafa192187ecd8e23abe2d65c9b0fe28
ezio84 <brabus84@gmail.com>
2017-01-02 09:53:08 +0100

    Battery: correctly colorize the "!" critical level percentage text
    When battery level is critical (<=5%), in-icon percentage is hidden and
    the system shows a warning exclamation mark "!".
    Without this commit, the warning symbol is always red. When PowerSaving
    is enabled, the status bar becomes orange (or red or the color the dev chose)
    so the symbol becomes invisible.
    With this commit, the symbol is painted accordingly to the power saver state
    (red if ps disabled, white if ps enabled) like the normal percentage
    level > critical level.
    
    Change-Id: Ia5ac954c3d8df01116c123b19c0a8073b54dbf17

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -125,7 +125,6 @@ public class BatteryMeterDrawable extends Drawable implements
     private boolean mInitialized;
 
     private Paint mTextAndBoltPaint;
-    private Paint mWarningTextPaint;
     private Paint mClearPaint;
 
     private LayerDrawable mBatteryDrawable;
@@ -200,12 +199,6 @@ public class BatteryMeterDrawable extends Drawable implements
         mTextAndBoltPaint.setTextAlign(getPaintAlignmentFromGravity(mTextGravity));
         mTextAndBoltPaint.setXfermode(new PorterDuffXfermode(xferMode));
         mTextAndBoltPaint.setColor(mBoltOverlay || mCurrentFillColor == 0 ? getBoltColor() : mCurrentFillColor);
-
-        mWarningTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mWarningTextPaint.setColor(mColors[1]);
-        font = Typeface.create("sans-serif", Typeface.BOLD);
-        mWarningTextPaint.setTypeface(font);
-        mWarningTextPaint.setTextAlign(getPaintAlignmentFromGravity(mTextGravity));
 
         mClearPaint = new Paint();
         mClearPaint.setColor(0);
@@ -549,7 +542,6 @@ public class BatteryMeterDrawable extends Drawable implements
                 break;
         }
         mTextAndBoltPaint.setTextSize(textSize);
-        mWarningTextPaint.setTextSize(textSize);
 
         Rect iconBounds = new Rect(0, 0, mWidth, mHeight);
         mBatteryDrawable.setBounds(iconBounds);
@@ -665,17 +657,18 @@ public class BatteryMeterDrawable extends Drawable implements
 
     private void drawPercentageText(Canvas canvas) {
         final int level = mLevel;
-        if (level > mCriticalLevel && mShowPercent && level != 100) {
+        if (mShowPercent && level != 100) {
             // Draw the percentage text
             String pctText = String.valueOf(SINGLE_DIGIT_PERCENT ? (level / 10) : level);
             mTextAndBoltPaint.setColor(getColorForLevel(level));
-            canvas.drawText(pctText, mTextX, mTextY, mTextAndBoltPaint);
+            if (level > mCriticalLevel) {
+                canvas.drawText(pctText, mTextX, mTextY, mTextAndBoltPaint);
+            } else {
+                canvas.drawText(mWarningString, mTextX, mTextY, mTextAndBoltPaint);
+            }
             if (mBoltOverlay) {
                 mBoltDrawable.setTint(getBoltColor());
             }
-        } else if (level <= mCriticalLevel) {
-            // Draw the warning text
-            canvas.drawText(mWarningString, mTextX, mTextY, mWarningTextPaint);
         }
     }
 
